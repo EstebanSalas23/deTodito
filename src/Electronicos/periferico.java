9bca30ca9319b31052a9ff4b638bc93cdbd9b369
EstebanSalas23 <estebansalasposada@gmail.com>
2025-09-17 15:54:15 -0500

    #subir archivos

---
+++ src/Electronicos/periferico.java
@@ -0,0 +1,27 @@
+
+package Electronicos;
+
+public abstract class periferico extends padreElec{
+    
+    protected String subCategorias;
+
+    public periferico(String nombre, String marca, String precio, String serial, String subCategorias) {
+        super(nombre, marca, precio, serial);
+        this.subCategorias = subCategorias;
+    }
+
+    public String getSubCategorias() {
+        return subCategorias;
+    }
+
+    public void setSubCategorias(String subCategorias) {
+        this.subCategorias = subCategorias;
+    }
+
+    @Override
+    public String toString() {
+        return "periferico{" + "subCategorias=" + subCategorias + '}';
+    }
+    
+    
+}
