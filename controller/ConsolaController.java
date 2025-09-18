ace48028c4bbd85647d6f355db2f1ab40a6da8a0
EstebanSalas23 <estebansalasposada@gmail.com>
2025-09-17 20:32:12 -0500

    actualizacion programa

---
+++ controller/ConsolaController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import modelo.Consola;
+
+public class ConsolaController {
+    
+    private List<Consola> consolas;
+    private int serial;
+
+    public ConsolaController() {
+        this.consolas = new ArrayList<>();
+        this.serial = 1;
+    }
+
+    public Boolean agregarConsola(String nombre, String marca, int precio, String cablePoder, String cableAux){
+
+        if (nombre == null || nombre.trim().isEmpty()){
+            return false;
+        }
+
+        if (precio <= 0){
+            return false;
+        }
+        
+        Consola consolaNuevo = new Consola();
+
+        Boolean agregado = consolas.add(consolaNuevo);
+        
+        if (agregado) serial++;
+
+        return agregado;
+    }
+}
